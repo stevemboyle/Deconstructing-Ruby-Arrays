5ace185dfb475ce34a3035fe7eec078fb826ce50
Steve Boyle <stevemboyle@Steves-MacBook-Air.local>
2016-05-19 12:38:27 -0700

    Initial Commit

---
+++ dynamic_array.rb
@@ -0,0 +1,120 @@
+require_relative "static_array"
+
+class DynamicArray
+  attr_reader :length
+
+	########################################
+	# Initialize
+	########################################
+
+  def initialize
+    self.store, self.capacity, self.length = StaticArray.new(8), 8, 0
+  end
+
+  ########################################
+	# GETTER:
+  # O(1) Time Complexity
+	########################################
+
+  def [](index)
+    check_index(index)
+    store[index]
+  end
+
+  ########################################
+  # SETTER:
+  # O(1) Time Complexity
+  ########################################
+
+  def []=(index, value)
+    check_index(index)
+    store[index] = value
+  end
+
+  ########################################
+	# POP:
+  # O(1) Time Complexity
+	########################################
+
+  def pop
+    raise "index out of bounds" unless (length > 0)
+
+    val, self[length - 1] = self[length - 1], nil
+    self.length -= 1
+
+    val
+  end
+
+  ########################################
+  # PUSH:
+  # O(1) Time Complexity Amortized
+  # O(n) Time Complexity in the Worst Case
+  # Variable Time Complexity Based on Rezizing
+  ########################################
+
+  def push(val)
+    resize! if length == capacity
+
+    self.length += 1
+    self[length - 1] = val
+
+    nil
+  end
+
+  ########################################
+  # SHIFT:
+  # O(n) Time Complexity
+  ########################################
+
+  def shift
+    raise "index out of bounds" if (length == 0)
+
+    val = self[0]
+    (1...length).each { |i| self[i - 1] = self[i] }
+    self.length -= 1
+
+    val
+  end
+
+
+  ########################################
+  # UNSHIFT:
+  # O(n) Time Complexity
+  ########################################
+
+
+  def unshift(val)
+    resize! if length == capacity
+
+    self.length += 1
+    (length - 2).downto(0).each { |i| self[i + 1] = self[i] }
+    self[0] = val
+
+    nil
+  end
+
+  protected
+  attr_accessor :capacity, :store
+  attr_writer :length
+
+  def check_index(index)
+    unless (index >= 0) && (index < length)
+      raise "index out of bounds"
+    end
+  end
+
+
+  ########################################
+  # RESIZE:
+  # O(n) Time Complexity
+  ########################################
+
+  def resize!
+    new_capacity = capacity * 2
+    new_store = StaticArray.new(new_capacity)
+    length.times { |i| new_store[i] = self[i] }
+
+    self.capacity = new_capacity
+    self.store = new_store
+  end
+end
---
+++ static_array.rb
@@ -0,0 +1,32 @@
+# This class just dumbs down a regular Array to be staticly sized.
+class StaticArray
+
+  ########################################
+  # Initialize
+  ########################################
+
+  def initialize(length)
+    self.store = Array.new(length, nil)
+  end
+
+  ########################################
+	# GETTER:
+  # O(1) Time Complexity
+	########################################
+
+  def [](index)
+    return store[index]
+  end
+
+  ########################################
+  # SETTER:
+  # O(1) Time Complexity
+  ########################################
+  
+  def []=(index, value)
+    store[index] = value
+  end
+
+  protected
+  attr_accessor :store
+end
